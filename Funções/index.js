1d72baf39531642289a8ad5d3e933165d950f5cb
Joel_RodriguesE <joelp.rodrigues25@gmail.com>
2024-01-16 21:43:55 -0300

    first commit

---
+++ Funções/index.js
@@ -0,0 +1,98 @@
+
+
+/*let number = 10
+
+const contagemRegressiva = setInterval(()=>{
+    console.log(number)
+
+    number--
+    if (number === 0){
+        console.log('Kabuuuum!')
+        clearInterval(contagemRegressiva)
+    } 
+}, 1000)*/
+
+//Exercicio Arrays + metodos
+
+/*const paises = ['Portugal', 'Brasil', 'Uruguai', 'China', 'Japão']
+
+console.log(paises)
+
+paises.push('Venezuela')
+console.log(paises)
+paises.pop()
+console.log(paises);
+paises.unshift('Coreia do norte')
+console.log(paises);
+paises.shift()
+console.log(paises);
+const ultimoPais = paises[paises.length - 1]
+console.log(ultimoPais)
+console.log(paises[1])
+console.log(paises[2])*/
+
+// Exercicio While and For 
+
+
+// Crie a variavel com 5 numeros 
+/*const numbers = [0 , 25, 56, 65, 12]
+//criando outra variavel para usar no parametro
+let i = 0
+
+while (i < numbers.length){
+    console.log(numbers[i])
+    i++
+}*/
+
+/*const numbers = [23, 14, 41, 54, 78, 911, 18]
+
+let sum = 0
+for (i = 0; i <numbers.length; i++){
+    sum += numbers[i]
+    console.log(sum)
+}*/
+
+/*const numbers = [12, 31, 51, 56, 67, 87];
+let sum = 0
+for(number of numbers){
+    sum += number
+
+    console.log(sum)
+}*/
+
+
+/*const palavra = 'Brasil'
+
+let encontrado = false
+
+for(let letra of palavra){
+    if(letra === 'h'){
+        console.log('tem a letra h')
+        encontrado = true
+        break}
+    }if(!encontrado){
+        console.log('não foi encontrado')
+    }*/
+
+
+/*const palavra = 'abracadabra'
+let soma = 0
+for (let letra of palavra){
+    if(letra === 'a'){
+        soma++
+        
+    }
+}
+console.log(soma)*/
+
+/*for (i = 10; i >= 0; i--){
+    console.log(i)
+}*/
+
+
+/*for (i = 0; i <=50; i+=2){
+    console.log(i)
+}*/
+
+
+
---
+++ Objetos/index.js
@@ -0,0 +1,79 @@
+
+
+/*const dados =
+ {nome:'Joel',
+idade: 25,
+altura: 1.83,
+CNH: false,
+apelido: 'JP'}
+
+
+let textoCnh = "";
+if(dados.CNH){
+    textoCnh = "Possui CNH"
+}else{
+    textoCnh = "Nao possui cnh"
+}
+
+console.log(`${dados.nome} tem ${dados.idade} anos, ${dados.altura} altura, ${textoCnh} e os seguintes apelidos ${dados.apelido}`)*/
+
+
+/*const cliente = {
+    nome: 'Julio',
+    idade: 26,
+    produtosConsumidos: [
+        {nome: 'pizza',
+         preçoUnitario: 1600,
+         quantidade: 2
+        },
+        {nome: 'cerveja',
+            preçoUnitario: 1800,
+            quantidade: 2
+        },
+        {nome: 'agua',
+            preçoUnitario: 600,
+            quantidade: 1
+        }
+       ]
+}
+
+console.log(cliente)*/
+
+
+/*const person = {
+    nome: "João",
+    idade: 12,
+    profissao: 'estudante',
+    altura: 1.4
+};
+
+function apresentacao (pessoa){
+    console.log(`Olá! meu nome é ${pessoa.nome}, sou um jovem de ${pessoa.idade}anos, tenho ${pessoa.altura}m, e sou ${pessoa.profissao}`)
+}
+
+apresentacao(person);*/
+
+
+/*function faixaEtaria (idade){
+    if( idade <= 21){
+        return "Jovem"
+    } else if(idade < 66){
+        return "Adulto"
+    } else {
+        return "Idoso"
+    }
+    
+}
+
+console.log(faixaEtaria(73))*/
+
+/*
+vOCÊ ESTÁ DESENVOLVENDO UM FORMULÁRIO DE CADASTRO E NOS PRIMEIROS TESTES DE USABILIDADE COM USUÁRIOS REAIS NOTOU ALGO PECULIAR: mUITOS USUÁRIOS PREENCHEM O FORMULÁRIO TODO COM A TECLA cAPS lOCK ATIVA, DEIXANDO TUDO BEM MENOS AGRADÁVEL DE LER. a SOLUÇÃO ESCOLHIDA NO dAILY DO DIA SEGUINTE FOI DE DETECTAR QUANDO O USUÁRIO ESTÁ ESCREVENDO DESSA FORMA E CORRIGIR AUTOMATICAMENTE. vOCÊ DEVE AGORA ESCREVER ESSE ALGORITMO.
+
+cONSIDERAMOS QUE UMA PALAVRA FOI ESCRITA COM cAPS lOCK SE OU TODAS AS SUAS LETRAS FOREM MAIÚSCULAS OU SE A PRIMEIRA FOR MINÚSCULA E TODAS AS OUTRAS MAIÚSCULAS. a CORREÇÃO NESSES CASOS É INVERTER TODAS AS LETRAS.
+
+*/
+
+    
+
+
